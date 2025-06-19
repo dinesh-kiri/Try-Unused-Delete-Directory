d27af47532d14cdf05fcdb2dda8f24482677987c
dinesh-kiri <dinesh.kiri.industries@gmail.com>
2025-06-19 16:27:37 +0530

    Update clean.ps1

--- clean.ps1
+++ clean.ps1
@@ -1,7 +1,7 @@
 # clean.ps1
 Write-Host "🚀 Auto Cleaner Started..."
 
-$scriptUrl = "https://github.com/dinesh-kiri/Try-Unused-Delete-Directory.git/cleaner.py"
+$scriptUrl = "https://github.com/dinesh-kiri/Try-Unused-Delete-Directory/blob/main/cleaner.py"
 $localFile = "$env:TEMP\cleaner.py"
 
 Invoke-WebRequest -Uri $scriptUrl -OutFile $localFile
